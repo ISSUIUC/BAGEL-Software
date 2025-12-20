fa3df0f15c85e56f8e27532d6a84641bd08a11c6
Thomas McManamen <tm38@illinois.edu>
2025-12-20 12:39:44 -0600

    initial hw test commit, has fw stm32 cube project with i2c scanner

---
+++ BAGEL/fw/Drivers/TCAL9538/TCAL9538.c
@@ -0,0 +1,8 @@
+/*
+ * TCAL9538.c
+ *
+ *  Created on: Dec 20, 2025
+ *      Author: tjmcm
+ */
+
+
---
+++ BAGEL/fw/Drivers/TCAL9538/TCAL9538.h
@@ -0,0 +1,13 @@
+/*
+ * TCAL9538.h
+ *
+ *  Created on: Dec 20, 2025
+ *      Author: tjmcm
+ */
+
+#ifndef TCAL9538_TCAL9538_H_
+#define TCAL9538_TCAL9538_H_
+
+
+
+#endif /* TCAL9538_TCAL9538_H_ */
